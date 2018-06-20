17374b6ebed80065a2048afbd94679c7fe4393fc
CarlosValdez7 <cgvaldez_@hotmail.com>
2018-06-19 19:18:42 -0600

    Modificando ejmplobase

--- ejmplobase.js
+++ ejmplobase.js
@@ -1,6 +1,6 @@
 let assert = require('chai').assert;
 
-describe('Caracteristica 1', function() {
+describe('Caracteristica 2', function() {
   it('Verifica que A = A ',function () {
     assert.equal('A','A');
   });
